473c153b9c51cd9f4a133bea4cd7db571090316b
Yihua Zhang <michaelzhang2@msn.com>
2019-05-05 17:17:35 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <h1> Monsters Rolodex </h1>
+        <h1>Monsters Rolodex</h1>
         <SearchBox onSearchChange={this.onSearchChange} />
         <CardList monsters={filteredMonsters} />
       </div>
