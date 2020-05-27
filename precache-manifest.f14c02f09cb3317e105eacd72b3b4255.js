14b41210b5f6283b351d0ba7532a58f80b21e5fc
Yihua Zhang <michaelzhang2@msn.com>
2020-05-27 12:46:33 -0400

    Updates

---
+++ precache-manifest.f14c02f09cb3317e105eacd72b3b4255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67940be99804c149057454d7cf49fddf",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "783631123ebd9553ea68",
+    "url": "/monsters-rolodex-complete/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-rolodex-complete/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "783631123ebd9553ea68",
+    "url": "/monsters-rolodex-complete/static/js/main.c2cd8eb4.chunk.js"
+  },
+  {
+    "revision": "683efa6afd61321d8334",
+    "url": "/monsters-rolodex-complete/static/js/runtime~main.0e367587.js"
+  }
+]);
